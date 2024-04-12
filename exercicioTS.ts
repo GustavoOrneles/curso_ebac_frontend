09a468669c7a8ed60b8ba0c6d0c43b9686790911
GustavoOrneles <Malwareby427@gmail.com>
2024-04-12 08:29:08 -0300

    Adicionando novo_arquivo.ts

---
+++ exercicioTS.ts
@@ -0,0 +1,13 @@
+function multiplicacao(number1: number, number2: number) {
+    return number1 * number2;
+}
+
+const resultado = multiplicacao(5, 10);
+console.log(resultado);
+
+function saudacao(nome: string) {
+    return 'Olá ' + nome;
+}
+
+const saudacaoParaGustavo = saudacao('Gustavo');
+console.log(saudacaoParaGustavo);
